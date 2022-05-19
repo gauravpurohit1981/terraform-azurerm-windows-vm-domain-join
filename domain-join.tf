f6766319ac8f7c0d267d5c07cb88543668ede887
Craig Thacker <craigthackerx@gmail.com>
2022-05-19 21:39:37 +0100

    Update

---
+++ domain-join.tf
@@ -0,0 +1,34 @@
+data "azurerm_virtual_machine" "fetch_vm" {
+  count               = var.lookup_vm_id == true ? 1 : 0
+  name                = var.vm_name
+  resource_group_name = var.rg_name
+}
+
+resource "azurerm_virtual_machine_extension" "domain_join" {
+
+  name = "ADDomainJoined"
+
+  virtual_machine_id   = var.lookup_vm_id == true ? element(values(data.azurerm_virtual_machine.fetch_vm.*.id), 0) : var.vm_id
+  publisher            = "Microsoft.Compute"
+  type                 = "JsonADDomainExtension"
+  type_handler_version = "1.3"
+  lifecycle {
+    prevent_destroy = true
+  }
+  settings           = <<SETTINGS
+      {
+      "Name": "${var.domain_name}",
+      "OUPath": "${var.ou_path}",
+      "User": "${var.domain_admin_username}@${var.domain_name}",
+      "Restart": "${lower(var.attempt_restart)}",
+      "Options": "3"
+      }
+  SETTINGS
+  protected_settings = <<PROTECTED_SETTINGS
+  {
+  "Password": "${var.domain_admin_password}"
+  }
+  PROTECTED_SETTINGS
+
+  tags = var.tags
+}
--- input.tf
+++ input.tf
@@ -1,18 +1,33 @@
-variable "identity_ids" {
-  description = "Specifies a list of user managed identity ids to be assigned to the VM."
-  type        = list(string)
-  default     = []
+variable "attempt_restart" {
+  type        = string
+  description = "Should this extension attempt a restart of the VM? This is recommended"
 }
 
-variable "identity_type" {
-  description = "The Managed Service Identity Type of this Virtual Machine."
+variable "domain_admin_password" {
   type        = string
-  default     = ""
+  description = "The domain admin password who has permission or delegation to perform the domain join"
 }
 
-variable "location" {
-  description = "The location for this resource to be put in"
+variable "domain_admin_username" {
   type        = string
+  description = "The domain admin username who has permission or delegation to perform the domain join"
+  sensitive   = true
+}
+
+variable "domain_name" {
+  type        = string
+  description = "The domain name of the domain you are trying to join, e.g. libredevops.org"
+}
+
+variable "lookup_vm_id" {
+  type        = string
+  description = "If a data source should be called to get the VM Id for you"
+  default     = false
+}
+
+variable "ou_path" {
+  type        = string
+  description = "The LDAP settings of the OU and groups of where you want this VM to be placed"
 }
 
 variable "rg_name" {
@@ -31,4 +46,14 @@ variable "tags" {
   default = {
     source = "terraform"
   }
+}
+
+variable "vm_id" {
+  type        = string
+  description = "The id of the virtual machine(s)"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "If lookup_vm_id is true, VM name must be set and the VM name must be provider"
 }
